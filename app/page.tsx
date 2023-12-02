a0de5554e354d0735271b2527d0c60d8b7cacea2
Yu Otsubo <yu.otsubo@eventhub.jp>
2023-12-02 15:32:26 +0900

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 "use client";
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Image from 'next/image'
 export default function Home() {
+  const [ loading, setLoading ] = useState(true);
 
   useEffect(() => {
     const timerId = setInterval(() => {
@@ -9,16 +10,17 @@ export default function Home() {
         if ('browsingTopics' in document &&
             //@ts-ignore
             document.featurePolicy.allowsFeature('browsing-topics')) {
-          //@ts-ignore
+            //@ts-ignore
             const topics = await document.browsingTopics();
             console.log(topics);
             console.log('🦜 document.browsingTopics():\n✅ Supported by browser\n✅ Allowed by Permissions Policy');
             console.log('Now calling await document.browsingTopics() ...');
+            setLoading(false);
         }
       })();
     }, 10000)
     return () => clearInterval(timerId)
-  }, [])
+  }, [loading])
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
